c8fc2de7cc8d8688bd9aa41356ea334bcf805530
Christian Peters <christianpeters31@outlook.com>
2021-05-12 15:12:59 -0400

    Added functionality to the number recommendation and fixed running issues

--- RandomNumberGuesser.py
+++ RandomNumberGuesser.py
@@ -22,19 +22,22 @@ def main():
 
     print("Welcome to the Random Number Generator!")
     print("The computer has generated a random number: ", machineRand)
-    userInput = input("Please guess the computer's random number: ")
-    userInput = int(userInput)
+    flag = True
+    while(flag):
+        userInput = input("Please guess the computer's random number: ")
+        userInput = int(userInput)
 
-    if (userInput == machineRand):
-        print("Congratulations! You have successfully guessed the computer's random number")
-        print("The Computer's random number was: ", machineRand)
-    else:
-        diff = abs(machineRand - userInput)
-        print("You did not guess the computer's random number, here are some hints!")
-        if(is_prime_number(machineRand)):
-            print("The Computer's random number is prime")
+        if (userInput == machineRand):
+            print("Congratulations! You have successfully guessed the computer's random number")
+            print("The Computer's random number was: ", machineRand)
+            flag = False
         else:
-            print("The Computer's random number is not prime")
+            diff = abs(machineRand - userInput)
+            print("You did not guess the computer's random number, here are some hints!")
+            if(is_prime_number(machineRand)):
+                print("The Computer's random number is prime")
+            else:
+                print("The Computer's random number is not prime")
 
 
 if __name__ == "__main__":
